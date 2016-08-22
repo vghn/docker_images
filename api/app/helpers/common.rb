aaa8b4df8f7f8e4bd394ce75b6927ca81cf7c03b
Vlad <vlad@ghn.me>
2016-08-22 12:24:00 -0500

    Output s3 sync commands

--- api/app/helpers/common.rb
+++ api/app/helpers/common.rb
@@ -36,14 +36,14 @@ end
 # Download vault
 def download_vault
   log.info 'Download vault'
-  `bash -c '. /opt/vpm/envrc && aws s3 sync "$VAULT_S3PATH" /etc/puppetlabs/vault/ --delete'`
+  puts `bash -c '. /opt/vpm/envrc && aws s3 sync "$VAULT_S3PATH" /etc/puppetlabs/vault/ --delete'`
   File.write('/var/local/deployed_vault', Time.now.localtime)
 end
 
 # Download Hiera data
 def download_hieradata
   log.info 'Download Hiera data'
-  `bash -c '. /opt/vpm/envrc && aws s3 sync "$HIERA_S3PATH" /etc/puppetlabs/hieradata/ --delete'`
+  puts `bash -c '. /opt/vpm/envrc && aws s3 sync "$HIERA_S3PATH" /etc/puppetlabs/hieradata/ --delete'`
   File.write('/var/local/deployed_hieradata', Time.now.localtime)
 end
 
