723c0836a48c1729ecdba98de18cb83aed19f1e2
Vlad <vlad@ghn.me>
2016-12-09 07:14:38 -0600

    Remove rack-ssl from api

--- vpm-api/app/spec/app_spec.rb
+++ vpm-api/app/spec/app_spec.rb
@@ -1,17 +1,6 @@
 require 'spec_helper'
 
 describe 'API' do
-  before(:each) do
-    env 'HTTPS', 'on'
-  end
-
-  it 'should redirect HTTP to HTTPS' do
-    env 'HTTPS', nil
-    get '/'
-    expect(last_response.redirect?).to be true
-    expect(last_response.headers['Location']).to eq('https://example.org/')
-  end
-
   it 'should allow accessing the home page' do
     get '/'
     expect(last_response).to be_ok
