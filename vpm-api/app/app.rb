f9cf138c5658263a4846f3258d60a212f7ae5572
Vlad <vlad@ghn.me>
2017-01-29 16:52:21 -0600

    Improve VPM API
    It should only start other docker compose services

--- vpm-api/app/app.rb
+++ vpm-api/app/app.rb
@@ -10,10 +10,8 @@ require 'sinatra'
 require 'yaml'
 
 # VARs
-CONFIG_FILE     = ENV['API_CONFIG']
-DATA_SERVICE    = ENV['DATA_SERVICE']
-SECRETS_SERVICE = ENV['SECRETS_SERVICE']
-CONTROL_REPO    = ENV['CONTROL_REPO']
+CONFIG  = ENV['API_CONFIG']
+SERVICE = ENV['SERVICE']
 
 # Logging
 def logger
@@ -22,92 +20,46 @@ end
 
 # Configuration
 def config
-  @config ||= YAML.load_file(CONFIG_FILE)
+  @config ||= YAML.load_file(CONFIG)
 end
 
 # Wait for the configuration
 def wait_for_config
-  if CONFIG_FILE
+  if CONFIG
     logger.info 'Wait for the configuration'
-    sleep 1 until File.exist?(CONFIG_FILE)
+    sleep 1 until File.exist?(CONFIG)
     logger.info 'Configuration found'
   else
     logger.warn 'Skip configuration because API_CONFIG is not set!'
   end
 end
 
+# Create a list of docker compose services
+def services
+  services ||= SERVICE.split(',') if SERVICE
+end
+
 # Get the data container ID
-def data_container_id(service)
+def get_container_id(service)
   @data_container_id ||= `docker ps --latest --all --filter \
     "label=com.docker.compose.service=#{service}" \
     --format "{{.ID}}"`.chomp
 end
 
-# Compose the volume arguments for the Docker command
-def docker_cmd
-  @docker_cmd = 'docker run --rm'
-
-  if DATA_SERVICE
-    @docker_cmd += " --volumes-from #{data_container_id DATA_SERVICE}"
-  end
-
-  return @docker_cmd
-end
-
 # Initial deployment
 def initial_deployment
   logger.info 'Start initial deployment'
-  deploy
+  start_services
   wait_for_config
 end
 
 # Deployment
-def deploy
-  deploy_secrets_thread
-  deploy_r10k_thread
-end
-
-# Deploy secrets
-def deploy_secrets_thread
-  if SECRETS_SERVICE
-    Thread.new { download_secrets }
-  else
-    logger.warn 'Skip downloading secrets because SECRETS_SERVICE is not set!'
-  end
-end
-
-# Deployment
-def deploy_r10k_thread
-  if CONTROL_REPO
-    Thread.new { deploy_r10k }
-  else
-    logger.warn 'Skip R10K deployment because CONTROL_REPO is not set!'
-  end
-end
-
-# Download secrets
-# '--volumes-from' is needed here because docker in docker does not work with
-# volumes mounted from the host
-def download_secrets
-  logger.info 'Download secrets'
-  if system "docker start #{data_container_id SECRETS_SERVICE}"
-    File.write('/var/local/deployed_secrets', Time.now.localtime)
-    logger.info 'Secrets downloaded'
-  else
-    logger.warn 'Failed to download secrets'
-  end
-end
-
-# Deploy R10K
-def deploy_r10k
-  logger.info 'Deploy R10K'
-  if system "#{docker_cmd} \
-    -e REMOTE='#{CONTROL_REPO}' \
-    vladgh/r10k r10k deploy environment --puppetfile"
-    File.write('/var/local/deployed_r10k', Time.now.localtime)
-    logger.info 'R10K deployed'
-  else
-    logger.warn 'Failed to deploy R10K'
+def start_services
+  services.each do |service|
+    Thread.new do
+      logger.info "Starting #{service}"
+      system "docker start #{get_container_id service}"
+    end
   end
 end
 
@@ -171,11 +123,11 @@ post '/travis' do
   payload = JSON.parse(params[:payload])
   verify_travis_signature(payload)
 
-  deploy
-  logger.info "Deployment requested from build ##{payload['number']} for the #{payload['branch']} " \
-              "branch of repository #{payload['repository']['name']}"
+  logger.info "Authorized request received from build ##{payload['number']} " \
+              "for the #{payload['branch']} branch " \
+              "of repository #{payload['repository']['name']}"
 
-  'Deployment started'
+  start_services
 end
 
 # GitHub webhook
@@ -184,10 +136,10 @@ post '/github' do
   verify_github_signature(request.body.read)
   payload = JSON.parse(params[:payload])
 
-  deploy
-  logger.info "Requested by GitHub user @#{payload['sender']['login']}"
+  logger.info 'Authorized request received from GitHub user ' \
+              "@#{payload['sender']['login']}"
 
-  'Deployment started'
+  start_services
 end
 
 # Slack slash command
@@ -199,7 +151,8 @@ post '/slack' do
   text    = params.fetch('text').strip
 
   if token == config['slack_token']
-    logger.info "Authorized request from slacker @#{user} on channel ##{channel}"
+    logger.info "Authorized request received from slacker @#{user} " \
+                "on channel ##{channel}"
   else
     log.warn "Unauthorized token received from slacker @#{user}"
   end
@@ -209,8 +162,8 @@ post '/slack' do
     case text
     when 'deploy'
       # Only use the threaded deployment because of the short timeout
-      deploy
-      'Deployment started in the background :thumbsup:'
+      start_services
+      'Services started in the background :thumbsup:'
     else
       "I don't understand '#{text}' :cry:"
     end
