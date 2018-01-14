716d8699655746b1fea82bcfddeb979ea419c512
Vlad <vlad@ghn.me>
2018-01-14 08:05:33 -0600

    Allow token from swarm secrets
    Closes #132

--- rs/entrypoint.sh
+++ rs/entrypoint.sh
@@ -13,6 +13,29 @@ SERVER_CERT="${SERVER_CERT:-/etc/ssl/certs/server-cert.pem}"
 SERVER_PORT="${SERVER_PORT:-10514}"
 REMOTE_LOGS_PATH="${REMOTE_LOGS_PATH:-/logs/remote}"
 LOGZIO_TOKEN="${LOGZIO_TOKEN:-}"
+LOGZIO_TOKEN_FILE="${LOGZIO_TOKEN_FILE:-}"
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+  local var="$1"
+  local fileVar="${var}_FILE"
+  local def="${2:-}"
+  if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+    echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+    exit 1
+  fi
+  local val="$def"
+  if [ "${!var:-}" ]; then
+    val="${!var}"
+  elif [ "${!fileVar:-}" ]; then
+    val="$(< "${!fileVar}")"
+  fi
+  export "$var"="$val"
+  unset "$fileVar"
+}
 
 # Make sure required files and directories exist
 mkdir -p /var/spool/rsyslog
@@ -71,6 +94,7 @@ RSYSLOG_CONF+=$'\n\n'
 RSYSLOG_CONF+="$RSYSLOG_SERVER"
 
 # Generate RSysLog Logz.io configuration
+file_env LOGZIO_TOKEN # Read env var or file
 read -r -d '' LOGZIO <<LOGZIO || true
 # Logz.io
 template(name="logzioFormat" type="string" string="[${LOGZIO_TOKEN}] <%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [type=syslog] %msg%\\n")
