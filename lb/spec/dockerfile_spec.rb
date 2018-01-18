028f4cabac0f63852aaba7699acbdca41563af9a
Vlad <vlad@ghn.me>
2018-01-18 12:00:35 -0600

    Improve tests
    Closes #133

--- lb/spec/dockerfile_spec.rb
+++ lb/spec/dockerfile_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
-DOCKER_IMAGE_DIRECTORY = File.dirname(File.dirname(__FILE__))
-
 describe 'Dockerfile' do
-  include Vtasks::Utils::DockerSharedContext::Container
+  before(:all) do
+    @image = ::Docker::Image.build_from_dir(File.dirname(File.dirname(__FILE__)))
+    set :backend, :docker
+    set :docker_image, @image.id
+  end
+
+  it "should have the maintainer label" do
+    expect(@image.json["Config"]["Labels"].has_key?("maintainer"))
+  end
 
   it 'uses the correct OS' do
     expect(os[:family]).to eq('alpine')
